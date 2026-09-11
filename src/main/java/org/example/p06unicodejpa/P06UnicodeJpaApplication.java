6713e6d4e2759371b1821a4f6c16696b3e93a46f
lindkvst <107998831+lindkvst@users.noreply.github.com>
2026-09-11 18:58:10 +0200

    init

---
+++ src/main/java/org/example/p06unicodejpa/P06UnicodeJpaApplication.java
@@ -0,0 +1,13 @@
+package org.example.p06unicodejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P06UnicodeJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P06UnicodeJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/p06unicodejpa/P06UnicodeJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.p06unicodejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P06UnicodeJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
